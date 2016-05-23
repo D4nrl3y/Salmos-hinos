44ba9d34fffbdf1afdd1272e2719e4d7cc1ff8f0
jonc <jonpchin@gmail.com>
2016-05-23 16:57:38 -0400

    updated readme, app will be on playstore tomorrow

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Go-Bible
-Android Bible App
+<b>Android Bible App<b><br>
+![Alt text](/img/homeBible.png?raw=true "Main Page")
+![Alt text](/img/readChapter.png?raw=true "Read Chapter")
+![Alt text](/img/searchChapter.png?raw=true "Search Chapter")
+<br>You can read the bible from anywhere with this simple bible app. Currently supports the ASV(American Standard Version) in English. In the future more versions and languages will be added. No ads. Forever free.
--- java/com/goplaychess/gobible/MainActivity.java
+++ java/com/goplaychess/gobible/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -222,20 +221,20 @@ public class MainActivity extends AppCompatActivity{
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+ //           return true;
+//        }
 
-        return super.onOptionsItemSelected(item);
-    }
+//        return super.onOptionsItemSelected(item);
+//    }
 
 
 }
